8351194c5fbbeb97adb3f21af1db60dfd2404c1f
Evgeniia Vdovichenko <e.vdovichenkova@gmail.com>
2023-10-16 11:25:25 +0200

    fix problems with form

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+  validates :Your_name, presence: true
+  validates :Event_title, presence: true
+  validates :Date, presence: true
+  validates :Description, presence: true
 end
